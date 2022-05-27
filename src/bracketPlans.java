53e95438cd906ee3570e57729829e5ba7299ab1f
Michael <44344017+mtangtran@users.noreply.github.com>
2022-05-26 21:47:37 -0400

    Updated comments on sample input and tested the question.

---
+++ src/bracketPlans.java
@@ -0,0 +1,22 @@
+
+
+import java.util.ArrayList;
+
+public class bracketPlans {
+
+    public static void main(String[] args){
+        System.out.println();
+    }
+
+    public boolean bracketCheck(String str){
+        boolean flag = false;
+
+        ArrayList<String> start = new ArrayList<String>();
+        ArrayList<String> end = new ArrayList<String>();
+
+
+
+        return flag;
+
+    }
+}
